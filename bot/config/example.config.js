c32536fc3293910c3c27c5b8f0d0cf280da20f7d
deadbear69 <iamadeadbear@gmail.com>
2023-03-20 18:55:33 +0600

    Initial commit

--- bot/config/example.config.js
+++ bot/config/example.config.js
@@ -1,18 +1,19 @@
 module.exports = {
 	settings: {
-		prefix: 'bot_prefix',
+		prefix: 'prefix',
         dev_mode: true,
         dashboard: false
 	},
 	secrets: {
-		main_client: 'main_client_id',
+		main_client: 'msin_client_id',
 		test_client: 'test_client_id',
-		server: 'test_server',
-		devs: ['dev_1_id', 'dev_2_id'],
+		server: 'server_id',
+		devs: ['devs', 'id', 'here'],
 
 	},
 	channels: {
-		log: 'log_channel_id'
+		log: 'log_channel',
+		privete_log: 'private_log'
 	},
 	colors: {
 		main: '#141414',
@@ -22,5 +23,17 @@ module.exports = {
 		green: '#00FF66',
 		yellow: '#FFF700',
 		white: '#FFFFFF',
+	},
+	emojis: {
+		cross: '<:zerocross:1046772196921585784>',
+		tick: '<:zerotick:1046772193830379610>',
+		setting: '<:zerosetting:1046772208590131270>',
+		bug: '<:zerobughunter:1046772169742491720>',
+		announce: '<:zeroannouncement:1046772187081736222>',
+		staff: '<:zerostaff:1046772160712146944>',
+		owner: '<:zerocrown:1046772157658697769>',
+		dev: '<:zerodev:1046772151786668032>',
+		guide: '<:zeroguide:1046772172745605140>',
+		bot: '<:zerobot:1046772179028688936>'
 	}
 }
